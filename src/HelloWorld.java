0d272d1e8cc78dc259cbb09838159791a4fd6bca
Gongjin Chen <kevinchen808@gmail.com>
2014-08-31 20:51:46 -0700

    new comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 	public static final void main(String []args) {
 		//this is comment
 		System.out.println("hello world!");
+		System.out.println("good git practice.");
 	}
 
 }
